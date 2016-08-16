65222f5fc71178b1bd6ecfb3246da67228af4667
kaihuasun@163.com <kaihuasun@163.com>
2016-08-16 13:25:17 +0800

    add flashhelper

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,11 +10,21 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.save
-    redirect_to groups_path
+    redirect_to groups_path, notice: "成功新建讨论版"
   end
 
   def show
     @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "更新成功"
   end
 
   private
