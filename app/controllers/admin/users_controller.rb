caa0fd34407709d04490b6f843748494b318c347
geophy <ligeophy@gmail.com>
2016-08-16 15:44:27 +0800

    add admin

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,39 @@
+class Admin::UsersController < ApplicationController
+
+  before_action :authenticate_user!
+  before_filter :require_is_admin
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:user_id] )
+  end
+
+
+  def update
+
+  end
+
+  def destroy
+    @user = User.find(params[:user_id] )
+    @user.destroy
+    redirect_to dmin_userss_path
+  end
+
+  def change
+    @user = User.find(params[:user_id] )
+    if @user.is_admin
+      @user.is_admin = false
+    else
+      @user.is_admin = true
+    end
+    @user.save
+    @users = User.all
+    render :index
+  end
+
+
+
+end
