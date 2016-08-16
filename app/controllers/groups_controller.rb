35e5e265fbb973162e26fa57d9e9ef6ccc83454e
kaihuasun@163.com <kaihuasun@163.com>
2016-08-16 12:40:50 +0800

    implement groups#new&createaction

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,20 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
