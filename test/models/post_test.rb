d0631f502afc3dbc43b68ba5d5c9cfbad56b436f
林哲伟 <lin@linzheweideMacBook-Pro.local>
2016-08-17 14:44:19 +0800

    install gem annotate

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  issue_id   :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
