fcf29b574271d057a352382b300f584617b142b6
林哲伟 <lin@linzheweideMacBook-Pro.local>
2016-08-17 15:26:54 +0800

    change migration table name

--- db/migrate/20160817045854_rename_group_id_to_issue_id.rb
+++ db/migrate/20160817045854_rename_group_id_to_issue_id.rb
@@ -1,4 +1,4 @@
-class RenameissueIdToIssueId < ActiveRecord::Migration[5.0]
+class RenameGroupIdToIssueId < ActiveRecord::Migration[5.0]
   def change
     rename_column :posts, :group_id, :issue_id
   end
