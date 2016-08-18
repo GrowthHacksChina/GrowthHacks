9f8591a5fe05578e536fa49f5bb5a9b57e483a3a
geophy <ligeophy@gmail.com>
2016-08-18 11:15:12 +0800

    rails db:migrate

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :integer          not null, primary key
+#  content     :text
+#  issue_id    :integer
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  title       :string
+#  author      :string
+#  issue       :string
+#  tag         :string
+#  origin_link :string
+#  PV          :string
+#  favorite    :string
+#
+
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
