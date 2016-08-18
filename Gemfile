c22cf1c5747bb35aea4d803cd367ca43bac1b346
linzhewei <zp8106@gmail.com>
2016-08-18 18:07:25 +0800

    finish search

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem "mini_magick"
 gem "will_paginate"
 gem "trix"
 gem "annotate"
+gem "ransack"
+gem "seo_helper", '~> 1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :authenticate_user!, only:[:new, :create, :edit, :destroy]
+  before_action :validate_search_key , :only => [:search]
 
   def index
     @posts = Post.all
@@ -54,12 +55,32 @@ class PostsController < ApplicationController
     redirect_to issue_path(@issue),alert:"Post deleted!"
   end
 
-
+  def search
+    if @query_string.present?
+      search_result = Post.ransack(@search_criteria).result(:distinct => true)
+      @posts = search_result.paginate(:page => params[:page], :per_page => 20 )
+      # set_page_title "搜索 #{@query_string}"
+      # @q = Post.ransack( params[:q])
+      # @posts = @q.result(distinct: true)
+    end
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content, :author, :tag, :origin_link, :PV)
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
   end
 
 end
