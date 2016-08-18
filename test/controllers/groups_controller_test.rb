50cf7e83d54796142a4d4773fce653ddab74605a
林哲伟 <lin@linzheweideMacBook-Pro.local>
2016-08-18 15:06:44 +0800

    change all [group] to [issue]

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class GroupsControllerTest < ActionDispatch::IntegrationTest
+class IssuesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class GroupTest < ActiveSupport::TestCase
+class IssueTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
