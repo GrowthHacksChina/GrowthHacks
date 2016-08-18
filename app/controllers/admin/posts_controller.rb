e4f79052dae9480f44858cabb39eee52b23e9cf8
geophy <ligeophy@gmail.com>
2016-08-18 17:04:02 +0800

    加入admin下管理issue和post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,37 +1,64 @@
 class Admin::PostsController < ApplicationController
-
   before_action :authenticate_user!
   before_action :require_is_admin
-  layout "admin"
+  layout 'admin'
 
   def index
     @posts = Post.all
   end
 
+  def show
+    @issue = Issue.find(params[:issue_id])
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def new
     @issue = Issue.find(params[:issue_id])
     @post = Post.new
   end
 
-  def new
-    @group = Group.find(params[:group_id])
-    @post = Post.new
-  end
-
   def create
-    @group = Group.find(params[:group_id])
-    @post =Post.new(post_params)
-    @post.group = @group
+    @issue = Issue.find(params[:issue_id])
+    @post = Post.new(post_params)
+    @post.issue = @issue
     @post.user = current_user
     if @post.save
-      redirect_to group_path(@group)
+      redirect_to admin_issue_path(@issue) ,notice: "Create Sueecss"
     else
-      render :new
+      render :new , notice: "Failed to create"
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to admin_post_path, notice: 'Update Success'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      redirect_to :back, notice: "Delete Success"
+    else
+      render :back, notice: "Failed to delete"
     end
   end
 
   private
+
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :title, :author)
   end
 end
