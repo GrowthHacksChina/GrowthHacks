af5d0775353e44b7bdaea470ebeb3c82ec52335e
geophy <ligeophy@gmail.com>
2016-08-22 16:10:37 +0800

    将master分支merge到develop分支

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :validate_search_key , :only => [:search]
 
   def index
     @jobs = Job.all
@@ -13,40 +14,31 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def create
-    @job = Job.new(job_params)
-    @job.user = current_user
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    @job = Job.find(params[:id])
-
-    if @job.update(job_params)
-      redirect_to jobs_path, notice: 'Update Success'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @job = Job.find(params[:id])
-
-    @job.destroy
-    redirect_to jobs_path, alert: 'Job deleted'
-  end
 
   private
 
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :work_address, :work_experience, :company, :company_description)
   end
+  def search
+    if @query_string.present?
+      search_result = Job.ransack(@search_criteria).result(:distinct => true)
+      @jobs = search_result.paginate(:page => params[:page], :per_page => 20 )
+      # set_page_title "搜尋 #{@query_string}"
+    end
+  end
+
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,50 @@
 class PostsController < ApplicationController
+
+  before_action :validate_search_key , :only => [:search]
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
 
   def index
     @posts = Post.all
   end
 
-  def edit
-    @issue = Issue.find(params[:issue_id])
-    @post = Post.new
-  end
 
-  def new
-    @issue = Issue.find(params[:issue_id])
-    @post = Post.new
-  end
 
   def show
     @issue = Issue.find(params[:issue_id])
     @post = Post.find(params[:id])
   end
 
-  def create
-    @issue = Issue.find(params[:issue_id])
-    @post = Post.new(post_params)
-    @post.issue = @issue
-    @post.user = current_user
-    if @post.save
-      redirect_to issue_path(@issue)
-    else
-      render :new
+
+
+
+  def search
+    if @query_string.present?
+      search_result = Post.ransack(@search_criteria).result(:distinct => true)
+      @posts = search_result.paginate(:page => params[:page], :per_page => 20 )
+      # set_page_title "搜索 #{@query_string}"
+      # @q = Post.ransack( params[:q])
+      # @posts = @q.result(distinct: true)
     end
   end
 
-  def destroy
-    @issue = Issue.find(params[:issue_id])
-    @post = Post.find(params[:id])
-
-    @post.destroy
-    redirect_to issue_path(@issue), alert: 'Post deleted!'
-  end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content, :author, :tag, :origin_link, :PV)
   end
+
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
+
 end
