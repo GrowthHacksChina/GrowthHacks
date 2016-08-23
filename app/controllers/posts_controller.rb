4f8e6734927182a62e60b422bd6b5740745253b7
kaihuasun@163.com <kaihuasun@163.com>
2016-08-23 19:31:03 +0800

    add pic to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,10 @@ class PostsController < ApplicationController
 
     private
 
-    def post_params
-        params.require(:post).permit(:title, :content, :author, :tag, :origin_link, :PV)
-    end
+    #
+    # def post_params
+    #     params.require(:post).permit(:title, :content, :author, :tag, :origin_link, :PV, :image)
+    # end
 
     protected
 
