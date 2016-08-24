df385e1683188ec7f30404b470a1f6cd4b653f5f
jone Doe <johndoe@example.com>
2016-08-24 09:39:33 +0800

    migrate

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,21 +2,22 @@
 #
 # Table name: posts
 #
-#  id          :integer          not null, primary key
-#  content     :text
-#  issue_id    :integer
-#  user_id     :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  title       :string
-#  author      :string
-#  issue       :string
-#  tag         :string
-#  origin_link :string
-#  PV          :string
-#  favorite    :string
-#  image       :string
+#  id                 :integer          not null, primary key
+#  content            :text
+#  issue_id           :integer
+#  user_id            :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  title              :string
+#  author             :string
+#  issue              :string
+#  tag                :string
+#  origin_link        :string
+#  pv                 :string
+#  favorite           :string
+#  image              :string
 #  brief_introduction :text
+#
 
 class Post < ApplicationRecord
     belongs_to :issue
