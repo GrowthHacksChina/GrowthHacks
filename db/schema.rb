6b93e5b3072a88c48c17dda2f47847a2cd852723
kaihuasun@163.com <kaihuasun@163.com>
2016-08-24 13:52:09 +0800

    show 2 issues in index

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,10 @@ ActiveRecord::Schema.define(version: 20160823092720) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "post_id"
-
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
 
   create_table "issue_relationships", force: :cascade do |t|
     t.integer  "issue_id"
@@ -40,7 +37,6 @@ ActiveRecord::Schema.define(version: 20160823092720) do
     t.string   "origin_link"
     t.string   "PV"
   end
-
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
