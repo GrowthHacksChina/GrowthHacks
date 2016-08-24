fd5a06c58eb867bee7f7f5fd2023d614491fc5df
kaihuasun@163.com <kaihuasun@163.com>
2016-08-24 20:02:47 +0800

    decorate account panel

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,42 @@
 class PostsController < ApplicationController
-    before_action :validate_search_key, only: [:search]
-    before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :validate_search_key, only: [:search]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
 
-    def index
-        @posts = Post.all
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @issue = @post.issue
+    drop_breadcrumb(@issue.title, issue_path(@issue))
+    drop_breadcrumb('正文')
+  end
+
+  def search
+    if @query_string.present?
+      search_result = Post.ransack(@search_criteria).result(distinct: true)
+      @posts = search_result.paginate(page: params[:page], per_page: 20)
+      # set_page_title "搜索 #{@query_string}"
+      # @q = Post.ransack( params[:q])
+      # @posts = @q.result(distinct: true)
     end
+  end
 
-    def show
-        @post = Post.find(params[:id])
-        @issue = @post.issue
-        drop_breadcrumb(@issue.title, issue_path(@issue))
-        drop_breadcrumb("本文")
-    end
+  private
 
-    def search
-        if @query_string.present?
-            search_result = Post.ransack(@search_criteria).result(distinct: true)
-            @posts = search_result.paginate(page: params[:page], per_page: 20)
-            # set_page_title "搜索 #{@query_string}"
-            # @q = Post.ransack( params[:q])
-            # @posts = @q.result(distinct: true)
-        end
-    end
+  def post_params
+    params.require(:post).permit(:title, :content, :author, :tag, :origin_link, :pv, :image, :brief_introduction)
+  end
 
-    private
+  protected
 
-    def post_params
-        params.require(:post).permit(:title, :content, :author, :tag, :origin_link, :pv, :image, :brief_introduction)
-    end
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, '') if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
 
-    protected
-
-    def validate_search_key
-        @query_string = params[:q].gsub(/\\|\'|\/|\?/, '') if params[:q].present?
-        @search_criteria = search_criteria(@query_string)
-    end
-
-    def search_criteria(query_string)
-        { title_cont: query_string }
-    end
+  def search_criteria(query_string)
+    { title_cont: query_string }
+  end
 end
