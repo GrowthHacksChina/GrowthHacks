d4a039f5af42ae52fec3f75a9e0c767f5d71e7a9
kaihuasun@163.com <kaihuasun@163.com>
2016-08-24 16:39:04 +0800

    set order to issue&posts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @issues = Issue.all
-    @posts = Post.all.paginate(:page => params[:page], :per_page => 5)
+    @issues = Issue.all.recent.paginate(page: params[:page], per_page: 10)
+    @posts = Post.all.paginate(page: params[:page], per_page: 5)
   end
 end
