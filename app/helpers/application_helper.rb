da5e7b8d4260674ba2f5582ed59cf691c6e8d8a4
kaihuasun@163.com <kaihuasun@163.com>
2016-08-26 16:44:04 +0800

    add banners

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,19 @@ module ApplicationHelper
   # end
 
   def render_markdown_text(text)
+    require 'redcarpet'
     text.to_markdown
   end
-
+  # def markdown(text)
+  #   require 'redcarpet'
+  #   render = Redcarpet::Render::HTML
+  #   options = {
+  #     autolink: true,
+  #     filter_html: true,
+  #     fenced_code_blocks: true,
+  #     no_intra_emphasis: true
+  #   }
+  #   markdown = Redcarpet::Markdown.new(render.new(hard_wrap: true), options)
+  #   markdown.render(text).html_safe
+  #   end
 end
