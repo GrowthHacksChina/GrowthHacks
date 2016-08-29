ba40ea94d1a90dfdb79058385ccf790a4238db13
xdite <xuite.joke@gmail.com>
2016-08-30 01:05:36 +0800

    fix rubocop

--- lib/bootstrap_helper/breadcrumb.rb
+++ lib/bootstrap_helper/breadcrumb.rb
@@ -8,7 +8,6 @@ module BootstrapHelper
     end
 
     module ClassMethods
-
     end
 
     module InstanceMethods
@@ -22,19 +21,19 @@ module BootstrapHelper
         @breadcrumbs = ["<a href='/admin/'>首页</a>".html_safe]
       end
 
-      def drop_breadcrumb(title=nil, url=nil)
+      def drop_breadcrumb(title = nil, url = nil)
         title ||= @page_title
 
         if title && url
           @breadcrumbs.push("<a href='#{url}'>#{title}</a>".html_safe)
         elsif title
-          @breadcrumbs.push("#{title}".html_safe)
+          @breadcrumbs.push(title.to_s.html_safe)
         end
       end
 
       def drop_page_title(title)
         @page_title = title
-        return @page_title
+        @page_title
       end
 
       def no_breadcrumbs
@@ -43,7 +42,6 @@ module BootstrapHelper
     end
 
     module Helpers
-
       def render_breadcrumb
         return "" if @breadcrumbs.size <= 0
         prefix = "".html_safe
@@ -51,16 +49,15 @@ module BootstrapHelper
 
         @breadcrumbs.each_with_index do |c, i|
           breadcrumb_class = []
-          breadcrumb_class << "first" if i == 0
+          breadcrumb_class << "first" if i.zero?
           breadcrumb_class << "last active" if i == (@breadcrumbs.length - 1)
 
           breadcrumb_content = c
 
-          crumb += content_tag(:li, breadcrumb_content ,:class => breadcrumb_class ) + "\n"
+          crumb += content_tag(:li, breadcrumb_content, class: breadcrumb_class) + "\n"
         end
-        return prefix + content_tag(:ul, crumb, :class => "breadcrumb menu clearfix")
+        prefix + content_tag(:ul, crumb, class: "breadcrumb menu clearfix")
       end
     end
   end
-
 end
