f454fc1210d37ff23f21c6282a98477c9007d33d
xdite <xuite.joke@gmail.com>
2016-08-30 01:16:52 +0800

    before_action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :require_is_admin
-  before_filter :set_admin_breadcrumbs
+  before_action :set_admin_breadcrumbs
   layout "admin"
 
   def set_admin_breadcrumbs
